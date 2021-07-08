af70cb523cb87b0dda279d9c6fee73ac81daded2
Ryhan <ryhan436@gmail.com>
2021-07-08 12:38:46 -0700

    increase scroll timeout

--- js/scroll.js
+++ js/scroll.js
@@ -9,7 +9,7 @@ const lscroll = new LocomotiveScroll({
 window.addEventListener('load', () =>{
 	setTimeout(() => {
         lscroll.update();   
-    }, 100);
+    }, 1000);
 });
 
 window.addEventListener("resize", ()=> {
